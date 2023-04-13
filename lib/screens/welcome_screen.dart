1192b7d927c84f7060c0e6e2d7690294fed51a63
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-13 13:19:45 +0200

    welcome screen was added

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(70.0),
+          child: const Text(
+            'Reakcje chemiczne',
+            style: TextStyle(color: Colors.black, fontSize: 25.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:saep_reakcje_organiczne/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(ChemicalApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
