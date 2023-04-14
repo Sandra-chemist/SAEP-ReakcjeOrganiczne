b8ec08b5a6cc525b152690de9037d52b5e0b042e
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-14 08:54:38 +0200

    classes was created in dart files

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Text('Logowanie'),
+    );
+  }
+}
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.red,
+      body: Text('rejestracja'),
+    );
+  }
+}
