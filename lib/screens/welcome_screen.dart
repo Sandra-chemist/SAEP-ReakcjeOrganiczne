d4868574b2a5329cfc909352b4d6de1498dbf189
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-14 12:10:57 +0200

    TextButton was changed into MaterialButton

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:saep_reakcje_organiczne/screens/login_screen.dart';
+import 'package:saep_reakcje_organiczne/screens/registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -14,38 +16,46 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 80.0),
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Row(
               children: const [
                 Text(
+                  textAlign: TextAlign.center,
                   'Reakcje chemiczne',
-                  style: TextStyle(color: Colors.black, fontSize: 25.0),
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25.0,
+                  ),
                 ),
               ],
             ),
-            Row(
-              children: [
-                TextButton(
+            Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Material(
+                  color: Colors.red,
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, LoginScreen.id);
+                    },
+                    child: const Text('Logowanie'),
+                  ),
+                )),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Material(
+                color: Colors.blue,
+                child: MaterialButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, 'login_screen');
-                  },
-                  child: const Text('Logowanie'),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, 'registration_screen');
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   child: const Text('Rejestracja'),
                 ),
-              ],
-            )
+              ),
+            ),
           ],
         ),
       ),
