96e99ecdc84538d8c076fed79fd5a51857b9c4e3
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-14 09:16:10 +0200

    code in welcome_screen was refactored

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -12,13 +12,20 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(70.0),
-          child: const Text(
-            'Reakcje chemiczne',
-            style: TextStyle(color: Colors.black, fontSize: 25.0),
-          ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 80.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: const [
+                Text(
+                  'Reakcje chemiczne',
+                  style: TextStyle(color: Colors.black, fontSize: 25.0),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
