cabbe05f363219446df44bbed5e88e6e043a95ea
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-18 15:34:29 +0200

    registration form was created

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -11,9 +11,44 @@ class RegistrationScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
-      body: Text('rejestracja'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              onChanged: (value) {},
+              decoration: const InputDecoration(
+                  hintText: 'E-mail',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0)),
+            ),
+            TextField(
+              onChanged: (value) {},
+              decoration: const InputDecoration(
+                  hintText: 'Hasło',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0)),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Material(
+                color: Colors.red,
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Zarejestruj się',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
