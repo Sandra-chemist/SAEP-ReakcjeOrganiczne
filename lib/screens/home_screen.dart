40bcc6f5582616b91374a4b54d83faf9a123ce78
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-19 12:55:00 +0200

    menu was added to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:saep_reakcje_organiczne/screens/welcome_screen.dart';
+import 'package:saep_reakcje_organiczne/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -35,9 +36,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.lightGreen,
         leading: null,
         actions: [
           IconButton(
@@ -48,7 +48,98 @@ class _HomeScreenState extends State<HomeScreen> {
               }),
         ],
       ),
-      body: SafeArea(child: Column()),
+      body: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                children: const [
+                  Text(
+                    'Reakcje w chemii organicznej!',
+                    textAlign: TextAlign.justify,
+                    style: kTitleStyle,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Material(
+                  color: Colors.lightGreen,
+                  borderRadius: BorderRadius.circular(25.0),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () {
+                      null;
+                    },
+                    child: Text(
+                      'NAUKA',
+                      style: TextStyle(
+                          fontFamily: 'Handlee',
+                          fontSize: 25.0,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Material(
+                  color: Colors.lightGreen,
+                  borderRadius: BorderRadius.circular(25.0),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () {
+                      null;
+                    },
+                    child: Text(
+                      'QUIZ',
+                      style: TextStyle(
+                          fontFamily: 'Handlee',
+                          fontSize: 25.0,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Material(
+                  color: Colors.lightGreen,
+                  borderRadius: BorderRadius.circular(25.0),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                    onPressed: () {
+                      null;
+                    },
+                    child: Text(
+                      'PUNKTY',
+                      style: TextStyle(
+                          fontFamily: 'Handlee',
+                          fontSize: 25.0,
+                          color: Colors.green[900],
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              const Text(
+                'Dr Sandra Skibiszewska',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color(0xFF1B5E20),
+                    fontFamily: 'Handlee',
+                    fontSize: 20.0),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
