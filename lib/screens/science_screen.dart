edebbec5051a91a33050419fd37116654ddace56
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-25 14:41:33 +0200

    skeleton of science screen was added

--- lib/screens/science_screen.dart
+++ lib/screens/science_screen.dart
@@ -15,7 +15,60 @@ class _ScienceScreenState extends State<ScienceScreen> {
     return Scaffold(
       appBar: BlueAppBar(),
       backgroundColor: Colors.white,
-      body: Column(),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Title(
+            color: Colors.black,
+            child: Text(
+              'Reakcja fenolu z jonami żelaza(III)',
+              style: TextStyle(fontFamily: 'Kalam', fontSize: 20.0),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Image(image: AssetImage('images/phenol.png')),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color(0XFF3a86ff),
+                ),
+                child: Center(
+                  child: Text(
+                    'Reakcja zachodzi',
+                    style: TextStyle(
+                      fontFamily: 'Kalam',
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Material(
+            color: Color(0XFF3a86ff),
+            borderRadius: BorderRadius.circular(20.0),
+            elevation: 5,
+            child: const MaterialButton(
+              onPressed: null,
+              child: Icon(
+                Icons.double_arrow_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+        ],
+      ),
     );
   }
 }
