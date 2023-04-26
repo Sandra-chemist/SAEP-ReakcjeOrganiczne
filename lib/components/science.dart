698695400a07e51bbe19d6dc6a263e415e617dfd
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-26 08:55:17 +0200

    two new classes was created

---
+++ lib/components/science.dart
@@ -0,0 +1,10 @@
+class Science {
+  late String titleReaction;
+  late String descriptionReaction;
+  late String reactionEquation;
+
+  Science(
+      {required this.titleReaction,
+      required this.descriptionReaction,
+      required this.reactionEquation});
+}
---
+++ lib/components/science_brain.dart
@@ -0,0 +1,14 @@
+import 'package:saep_reakcje_organiczne/components/science.dart';
+
+class ScienceBrain {
+  final List<Science> scienceBank = [
+    Science(
+        titleReaction: 'Reakcja fenolu z jonami żelaza(III)',
+        descriptionReaction: 'Reakcja zachodzi',
+        reactionEquation: 'images/phenol.png'),
+    Science(
+        titleReaction: 'Reakcja glikolu z wodorotlenkiem miedzi(II)',
+        descriptionReaction: 'dffffdf',
+        reactionEquation: 'images/flask_2.png'),
+  ];
+}
