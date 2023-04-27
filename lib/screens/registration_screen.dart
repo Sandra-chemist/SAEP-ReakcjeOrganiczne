8a188b20a7b3060aee5df1db96b1fae47d469939
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-27 12:11:27 +0200

    refactoring and cleaning registration screen code

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:saep_reakcje_organiczne/methods.dart';
 import 'package:saep_reakcje_organiczne/screens/home_screen.dart';
 import 'package:saep_reakcje_organiczne/components/blue_app_bar.dart';
 import 'package:saep_reakcje_organiczne/constants.dart';
@@ -29,11 +30,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Column(
-              children: const [
-                SizedBox(
-                  height: 50.0,
-                ),
-                Text(
+              children: [
+                addSizeBox(50),
+                const Text(
                   'REJESTRACJA',
                   textAlign: TextAlign.justify,
                   style: kTitleStyle,
@@ -41,42 +40,28 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               ],
             ),
             TextField(
-              style: const TextStyle(color: Colors.white),
+              style: kHintTextStyle,
               textAlign: TextAlign.center,
-              decoration: const InputDecoration(
-                hintText: 'E-mail',
-                hintStyle: TextStyle(color: Colors.white),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                enabledBorder: kUnderlineInputBorder,
-              ),
+              decoration: addInputDecoration('E-mail'),
               onChanged: (value) {
                 password = value;
               },
             ),
             TextField(
-              style: const TextStyle(color: Colors.white),
+              style: kHintTextStyle,
               textAlign: TextAlign.center,
               obscureText: true,
-              decoration: const InputDecoration(
-                hintText: 'Hasło',
-                hintStyle: TextStyle(color: Colors.white),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                enabledBorder: kUnderlineInputBorder,
-              ),
+              decoration: addInputDecoration('Hasło'),
               onChanged: (value) {
                 password = value;
               },
             ),
-            const SizedBox(
-              height: 10.0,
-            ),
+            addSizeBox(10),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: Material(
                 borderRadius: BorderRadius.circular(25.0),
-                color: const Color(0xFFbde0fe),
+                color: kRoundedButtonColor,
                 child: MaterialButton(
                   onPressed: () async {
                     try {
@@ -101,13 +86,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 image: AssetImage('images/flask_2.png'),
               ),
             ),
-            const SizedBox(
-              height: 5.0,
-            ),
+            addSizeBox(5),
             kAuthorName,
-            const SizedBox(
-              height: 10.0,
-            )
+            addSizeBox(10),
           ],
         ),
       ),
