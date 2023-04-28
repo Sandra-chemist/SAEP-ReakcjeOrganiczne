f12a21dde06eecfd1c924765d8588fa7ac22b2a9
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2023-04-28 09:31:35 +0200

    modal progress was added to login and register

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:saep_reakcje_organiczne/constants.dart';
 import 'package:saep_reakcje_organiczne/methods.dart';
 import 'package:saep_reakcje_organiczne/screens/home_screen.dart';
 import 'package:saep_reakcje_organiczne/components/blue_app_bar.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -15,6 +16,7 @@ class LoginScreen extends StatefulWidget {
 
 class LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   late String email;
   late String password;
 
@@ -23,73 +25,86 @@ class LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: BlueAppBar(),
       backgroundColor: kBackgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Column(
-              children: [
-                addSizeBox(40),
-                const Text(
-                  'LOGOWANIE',
-                  textAlign: TextAlign.justify,
-                  style: kTitleStyle,
-                ),
-              ],
-            ),
-            TextField(
-              style: kHintTextStyle,
-              textAlign: TextAlign.center,
-              decoration: addInputDecoration('E-mail'),
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            TextField(
-              style: kHintTextStyle,
-              textAlign: TextAlign.center,
-              obscureText: true,
-              decoration: addInputDecoration('Hasło'),
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            addSizeBox(10),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                borderRadius: BorderRadius.circular(25.0),
-                color: const Color(0xFFbde0fe),
-                child: MaterialButton(
-                  onPressed: () async {
-                    try {
-                      final newUser = await _auth.signInWithEmailAndPassword(
-                          email: email, password: password);
-                      if (newUser != null) {
-                        Navigator.pushNamed(context, HomeScreen.id);
+      body: ModalProgressHUD(
+        color: Colors.black,
+        progressIndicator: const CircularProgressIndicator(
+          color: Colors.white,
+        ),
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                children: [
+                  addSizeBox(40),
+                  const Text(
+                    'LOGOWANIE',
+                    textAlign: TextAlign.justify,
+                    style: kTitleStyle,
+                  ),
+                ],
+              ),
+              TextField(
+                style: kHintTextStyle,
+                textAlign: TextAlign.center,
+                decoration: addInputDecoration('E-mail'),
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              TextField(
+                style: kHintTextStyle,
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: addInputDecoration('Hasło'),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              addSizeBox(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  borderRadius: BorderRadius.circular(25.0),
+                  color: const Color(0xFFbde0fe),
+                  child: MaterialButton(
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner = true;
+                      });
+                      try {
+                        final newUser = await _auth.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        if (newUser != null) {
+                          Navigator.pushNamed(context, HomeScreen.id);
+                        }
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      } catch (e) {
+                        print(e);
                       }
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
-                  child: const Text(
-                    'Zaloguj',
-                    style: kMaterialButtonStyle,
+                    },
+                    child: const Text(
+                      'Zaloguj',
+                      style: kMaterialButtonStyle,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Expanded(
-              child: Image(
-                image: AssetImage('images/flask_1.png'),
+              const Expanded(
+                child: Image(
+                  image: AssetImage('images/flask_1.png'),
+                ),
               ),
-            ),
-            addSizeBox(5),
-            kAuthorName,
-            addSizeBox(10),
-          ],
+              addSizeBox(5),
+              kAuthorName,
+              addSizeBox(10),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -4,6 +4,7 @@ import 'package:saep_reakcje_organiczne/methods.dart';
 import 'package:saep_reakcje_organiczne/screens/home_screen.dart';
 import 'package:saep_reakcje_organiczne/components/blue_app_bar.dart';
 import 'package:saep_reakcje_organiczne/constants.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration_screen';
@@ -15,6 +16,7 @@ class RegistrationScreen extends StatefulWidget {
 
 class RegistrationScreenState extends State<RegistrationScreen> {
   final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
   late String email;
   late String password;
 
@@ -23,74 +25,87 @@ class RegistrationScreenState extends State<RegistrationScreen> {
     return Scaffold(
       appBar: BlueAppBar(),
       backgroundColor: kBackgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Column(
-              children: [
-                addSizeBox(40),
-                const Text(
-                  'REJESTRACJA',
-                  textAlign: TextAlign.justify,
-                  style: kTitleStyle,
-                ),
-              ],
-            ),
-            TextField(
-              style: kHintTextStyle,
-              textAlign: TextAlign.center,
-              decoration: addInputDecoration('E-mail'),
-              onChanged: (value) {
-                email = value;
-              },
-            ),
-            TextField(
-              style: kHintTextStyle,
-              textAlign: TextAlign.center,
-              obscureText: true,
-              decoration: addInputDecoration('Hasło'),
-              onChanged: (value) {
-                password = value;
-              },
-            ),
-            addSizeBox(10),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                borderRadius: BorderRadius.circular(25.0),
-                color: kRoundedButtonColor,
-                child: MaterialButton(
-                  onPressed: () async {
-                    try {
-                      final newUser =
-                          await _auth.createUserWithEmailAndPassword(
-                              email: email, password: password);
-                      if (newUser != null) {
-                        Navigator.pushNamed(context, HomeScreen.id);
+      body: ModalProgressHUD(
+        color: Colors.black,
+        progressIndicator: const CircularProgressIndicator(
+          color: Colors.white,
+        ),
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                children: [
+                  addSizeBox(40),
+                  const Text(
+                    'REJESTRACJA',
+                    textAlign: TextAlign.justify,
+                    style: kTitleStyle,
+                  ),
+                ],
+              ),
+              TextField(
+                style: kHintTextStyle,
+                textAlign: TextAlign.center,
+                decoration: addInputDecoration('E-mail'),
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              TextField(
+                style: kHintTextStyle,
+                textAlign: TextAlign.center,
+                obscureText: true,
+                decoration: addInputDecoration('Hasło'),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              addSizeBox(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Material(
+                  borderRadius: BorderRadius.circular(25.0),
+                  color: kRoundedButtonColor,
+                  child: MaterialButton(
+                    onPressed: () async {
+                      setState(() {
+                        showSpinner = true;
+                      });
+                      try {
+                        final newUser =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        if (newUser != null) {
+                          Navigator.pushNamed(context, HomeScreen.id);
+                        }
+                        setState(() {
+                          showSpinner = false;
+                        });
+                      } catch (e) {
+                        print(e);
                       }
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
-                  child: const Text(
-                    'Rejestruj',
-                    style: kMaterialButtonStyle,
+                    },
+                    child: const Text(
+                      'Rejestruj',
+                      style: kMaterialButtonStyle,
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Expanded(
-              child: Image(
-                image: AssetImage('images/flask_2.png'),
+              const Expanded(
+                child: Image(
+                  image: AssetImage('images/flask_2.png'),
+                ),
               ),
-            ),
-            addSizeBox(5),
-            kAuthorName,
-            addSizeBox(10),
-          ],
+              addSizeBox(5),
+              kAuthorName,
+              addSizeBox(10),
+            ],
+          ),
         ),
       ),
     );
